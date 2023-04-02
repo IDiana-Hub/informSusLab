9b999ca08079e1db73006a806dc48453bbe7308e
IDiana-Hub <64288498+IDiana-Hub@users.noreply.github.com>
2023-04-02 20:19:58 +0300

    start
    login and parol

---
+++ InformSusLabClient/src/layouts/default/Default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-sheet width="300" class="mx-auto">
+    <h1>Авторизація</h1>
+    
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="loginvalue"
+        :rules="rules"
+        label="Логін"
+      ></v-text-field>
+      <v-text-field
+        v-model="parolvalue"
+        :rules="rules"
+        label="Логін"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2">Відправити</v-btn>
+    </v-form>
+  </v-sheet>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      loginvalue: '',
+      rules: [
+        value => {
+          if (value) return true
+
+          return "Ім'я не маоже бути порожнім"
+        },
+      ],
+    }),
+  }
+</script>
